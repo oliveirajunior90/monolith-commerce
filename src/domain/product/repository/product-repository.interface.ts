37dc61ece268fd99c52d52f96915336d3bb0b7ea
Almir Jr <almir.junior@olxbr.com>
2023-02-09 19:17:44 -0300

    feat: product-admin module and core

--- src/domain/product/repository/product-repository.interface.ts
+++
@@ -1,5 +0,0 @@
-import RepositoryInterface from "../../@shared/repository/repository-interface";
-import ProductInterface from "../entity/product.interface";
-
-export default interface ProductRepositoryInterface
-  extends RepositoryInterface<ProductInterface> {}
